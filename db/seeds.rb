476a16678a93150ee83d783142214fdff881fa10
Ines de Sousa Oliveira <inessoliveir@gmail.com>
2021-11-10 16:37:59 +0000

    form

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do
-  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, stars: rand(0..5), chef_name: Faker::Name.name)
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
   puts "Created a restaurant named #{restaurant.name}"
 end
